33b29ebceedd4994cafc81c0bf46bd3004384577
TAKAMURA Tomoe <hotkafeore@gmail.com>
2026-09-17 14:18:23 +0900

    ジャンプ機能を追加

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,23 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour //クラス名をPlayerMovementに変更すること
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 8f;
 
     private Rigidbody2D rb;
+    private bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (isGrounded && Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+            isGrounded = false;
+        }
     }
 
     private void FixedUpdate()
@@ -26,5 +37,13 @@ public class PlayerMovement : MonoBehaviour //クラス名をPlayerMovementに�
         }
 
         rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
